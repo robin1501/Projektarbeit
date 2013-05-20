2affa60955c1dd179725f9b0feb47260731df815
robin_1501 <krietscr@dhbw-loerrach.de>
2013-05-20 20:48:02 +0200

    Logineingabe prüfen + Daten aus CSV Datei zurückgeben

--- src/data/Load.java
+++ src/data/Load.java
@@ -1,17 +1,42 @@
 package data;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 /**
  * 
- * Grundsäzliche Load Klasse was hier drin passiert ob mit oder ohne Sortierung muss von Robin und NIls geklärt werden.
- * Außerdem muss hier der Aufbau der neunen CSV datei "NOTEN" geklärt werden.
+ * Grundsäzliche Load Klasse was hier drin passiert ob mit oder ohne Sortierung
+ * muss von Robin und NIls geklärt werden. Außerdem muss hier der Aufbau der
+ * neunen CSV datei "NOTEN" geklärt werden.
  * 
- *
+ * 
  */
 public class Load {
 
-	public static ArrayList<String> TryLoadUser(String userID, char[] pswd) {
+	static String user;
+	static String pw;
+	static String line;
+
+	static String[] column = new String[50];
+	
+	static ArrayList<String> data = new ArrayList<String>();
+
+	static boolean loginAccepted = false;
+
+	/*
+	 * public Load(String user, char[] pswd) {
+	 * 
+	 * this.pswd = pswd; this.user = user;
+	 * 
+	 * }
+	 */
+
+	public static ArrayList<String> TryLoadUser(String userID, char[] pswd) throws FileNotFoundException {
 		
 		// wenn laden möglich, dann bitte die ArrayList mit allen daten die man für den Konstruktor einer rolle Braucht mitgeben und zwar in der Richtigen Reihenfolge!  SONST NULL
 		// bitte den Rollen entnehmen !!! wichtig
@@ -23,9 +48,48 @@ public class Load {
 		 * und falsches passwort
 		 * 
 		 */
-		return null;
+		
+		FileReader fr = new FileReader("");
+	    BufferedReader br = new BufferedReader(fr);
+
+	    try {
+	    	
+			while((line = br.readLine()) != null && !loginAccepted)
+			{
+				column = line.split(";");
+				
+				if(column[3] == userID)
+				{
+					if(column[4] == pswd)
+					{
+						//string role, String isHead, String name,String firstname, String id, String course
+						data.add(column[2]);	//Rolle
+						data.add(column[6]);	//Studiengang
+						data.add(column[0]);	//Nachname
+						data.add(column[1]);	//Vorname
+						data.add(column[3]);	//ID
+						data.add(column[5]);	//Studiengang
+						
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null, "Falsches Passwort eingegeben", "Login Error", JOptionPane.ERROR_MESSAGE);
+					}
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Benutzername inkorrekt", "Login Error", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		
+		}
+		
+		return data;
 		
 		
 	}
-
 }
