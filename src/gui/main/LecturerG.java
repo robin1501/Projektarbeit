946c62457bf242fb93951905dc704289023fd7f2
DTRaich <trefzeda@dhbw-loerrach.de>
2013-06-21 11:12:21 +0200

    Close event eingebunden ---> erkennt nur keine änderungen wie beim Speichern auch (robin)

--- src/gui/main/LecturerG.java
+++ src/gui/main/LecturerG.java
@@ -24,6 +24,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.ArrayList;
@@ -86,7 +87,14 @@ public class LecturerG extends JFrame implements IDisposeMe {
 	 * Create the frame.
 	 */
 	public LecturerG() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent event) {
+				disposeMeFromExtern();
+			}
+		});
+
 		setResizable(false);
 		setTitle("Lehrk\u00F6rper-Hauptansicht");
 		setBounds(100, 100, 1103, 466);
